cb7a7f8e36272368e3940afa425c1b814a797d6c
Henrik Weisberg Andersen <HEWA@flsmidth.com>
2025-10-08 08:07:49 +0200

    Finalized should serialize Seq

--- src/SparkplugNet/VersionB/ProtoBuf/ProtobufPayload.ShouldSerialize.cs
+++ src/SparkplugNet/VersionB/ProtoBuf/ProtobufPayload.ShouldSerialize.cs
@@ -3,6 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+/// <summary>
+/// This partial class implements ShoudSerializeSeq which is call when serializing.
+/// It determines whether or not the Seq property shourl be included in the serialized payload
+/// For NDEATH messages the Seq property should be omitted accordign to the 3.0 specification.
+/// For all other message types the Seq property should be included. Especially seq = 0 for NBIRTH messages. Required for compliance with Ignition.
+/// </summary>
 internal partial class ProtoBufPayload
 {
     public SparkplugMessageType MessageType { get; set; }
